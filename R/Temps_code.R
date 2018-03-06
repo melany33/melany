847540d9c3f6bd285975ba9f46907bbf311bfdaa
melany33 <36862504+melany33@users.noreply.github.com>
2018-03-06 14:00:56 +0100

    Rccp tout marche

--- R/Temps_code.R
+++
@@ -1,2 +0,0 @@
-n<-10e4
-pdfval <- mvnpdf(x=matrix(1.96, nrow = 2, ncol = n), Log=FALSE)
--- src/file1.cpp
+++ src/file1.cpp
@@ -10,18 +10,9 @@ using namespace Rcpp;
 //   http://adv-r.had.co.nz/Rcpp.html
 //   http://gallery.rcpp.org/
 //
-
+//' @export
 // [[Rcpp::export]]
 NumericVector timesTwo(NumericVector x) {
   return x * 2;
 }
 
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
